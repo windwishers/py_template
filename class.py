4ced7dbc05b4a462b075e5f0da5b9a3ddf797504
windwishers <shadowog@gmail.com>
2016-10-18 17:58:38 +0900

    Initial commit

---
+++ class.py
@@ -0,0 +1,22 @@
+# noinspection PyStatementEffect
+class ClassName:
+
+    # 생성자
+    def __init__(self):
+        None
+
+    # 소멸자
+    def __del__(self):
+        None
+
+    # 이항 더하기 연산 함수
+    def __add__(self, other):
+        None
+
+    # 단항 더하기 연산
+    def __pos__(self):
+        None
+
+
+
+
